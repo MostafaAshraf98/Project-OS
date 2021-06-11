4c9846549b022e61d20390672ced337b081b7e3f
ahmoody2000 <58375227+ahmoody2000@users.noreply.github.com>
2021-06-11 16:22:47 +0200

    Added the input file reading in process_generator.c and added processes.txt file which was generated by test_generator.c

--- code/process_generator.c
+++ code/process_generator.c
@@ -2,11 +2,65 @@
 
 void clearResources(int);
 
+struct Processes
+{
+    int ID;
+    int arrivalTime;
+    int runningTime;
+    int priority;
+};
+
+typedef struct Processes Processes;     //to declare Process without writing struct
+
 int main(int argc, char *argv[])
 {
     signal(SIGINT, clearResources);
     // TODO Initialization
     // 1. Read the input files.
+    FILE* inputFile;
+    inputFile = fopen("processes.txt", "r");    //Opens file with parameters (path, reading only)
+    //Parameters that will be readed from the input file
+    int inputFileID;
+    int inputFileArrTime;
+    int inputFileRunTime;
+    int inputFilePriority;
+
+    
+    char buff[255];
+    fgets(buff,255,inputFile);  //This reads the first line "#id arrival runtime priority" which is not important
+    
+    while ( fscanf( inputFile, "%d\t%d\t%d\t%d\n", &inputFileID, &inputFileArrTime, &inputFileRunTime, &inputFilePriority) != EOF )
+    {
+        //This while loop is used to get the number of processes in the file to create an array
+    }
+
+    //inputFileId is the number of process
+    Processes fileProcesses[inputFileID];
+    int fileProcessesCount = 0;
+
+    fclose(inputFile);  //Closes the inputFile
+
+    
+
+    inputFile = fopen("processes.txt", "r");    //Opens file with parameters (path, reading only)
+
+    inputFile = fopen("processes.txt", "r");    //Opens file with parameters (path, reading only)
+    fgets(buff,255,inputFile);  //This reads the first line "#id arrival runtime priority" which is not important
+    while ( fscanf( inputFile, "%d\t%d\t%d\t%d\n", &inputFileID, &inputFileArrTime, &inputFileRunTime, &inputFilePriority) != EOF )
+    {
+        //With every loop, the values of inputFileID, inputFileArrTime, inputFileRunTime and inputFilePriority changes
+        //The values are added to the array fileProcesses
+        Processes p;
+        p.ID            = inputFileID;
+        p.arrivalTime   = inputFileArrTime;
+        p.runningTime   = inputFileRunTime;
+        p.priority      = inputFilePriority;
+        fileProcesses[fileProcessesCount++] = p;
+    }
+    
+    //NOW we have an array of Processes "fileProcesses" with size "fileProcessesCount" that contains the parameters of each process
+    
+
     // 2. Read the chosen scheduling algorithm and its parameters, if there are any from the argument list.
     // 3. Initiate and create the scheduler and clock processes.
     // 4. Use this function after creating the clock process to initialize clock.
