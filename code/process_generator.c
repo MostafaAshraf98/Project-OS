e2a7734b196de2f4d0096a8bb706f71fd9c70982
ahmoody2000 <58375227+ahmoody2000@users.noreply.github.com>
2021-06-13 02:01:26 +0200

    Updated the file reader to support comments and take the filename as an argument

--- code/process_generator.c
+++ code/process_generator.c
@@ -1,54 +1,71 @@
 #include "headers.h"
 #include "PriorityQueue.h"
-
+#include <time.h>
 void clearResources(int);
-
 
 int main(int argc, char *argv[])
 {
     signal(SIGINT, clearResources);
     // TODO Initialization
     // 1. Read the input files.
-    FILE* inputFile;
-    inputFile = fopen("processes.txt", "r");    //Opens file with parameters (path, reading only)
+    FILE *inputFile;
+    char* inputFileName = argv[1];
+    inputFile = fopen(inputFileName, "r"); //Opens file with parameters (path, reading only)
     //Parameters that will be readed from the input file
     int inputFileID;
     int inputFileArrTime;
     int inputFileRunTime;
     int inputFilePriority;
 
-    
-    char buff[255];
-    fgets(buff,255,inputFile);  //This reads the first line "#id arrival runtime priority" which is not important
-    
-    while ( fscanf( inputFile, "%d\t%d\t%d\t%d\n", &inputFileID, &inputFileArrTime, &inputFileRunTime, &inputFilePriority) != EOF )
+    int count = 0;
+    char c;
+    c = fgetc(inputFile);
+    while (c != EOF)
     {
-        //This while loop is used to get the number of processes in the file to create an array
+        if (c == '#')
+        {
+            char buff[255];
+            fgets(buff, 255, inputFile);
+        }
+        else
+        {
+            count++;
+        }
+        c = fgetc(inputFile);
     }
 
     //inputFileId is the number of process
-    process fileProcesses[inputFileID];
+    process fileProcesses[count];
     int fileProcessesCount = 0;
 
-    fclose(inputFile);  //Closes the inputFile
+    fclose(inputFile); //Closes the inputFile
 
-    
+    inputFile = fopen(inputFileName, "r"); //Opens file with parameters (path, reading only)
 
-    inputFile = fopen("processes.txt", "r");    //Opens file with parameters (path, reading only)
-
-    fgets(buff,255,inputFile);  //This reads the first line "#id arrival runtime priority" which is not important
-    while ( fscanf( inputFile, "%d\t%d\t%d\t%d\n", &inputFileID, &inputFileArrTime, &inputFileRunTime, &inputFilePriority) != EOF )
+    c = fgetc(inputFile);
+    while (c != EOF)
     {
         //With every loop, the values of inputFileID, inputFileArrTime, inputFileRunTime and inputFilePriority changes
         //The values are added to the array fileProcesses
-        process p;
-        p.id            = inputFileID;
-        p.arrivalTime   = inputFileArrTime;
-        p.runTime       = inputFileRunTime;
-        p.priority      = inputFilePriority;
-        fileProcesses[fileProcessesCount++] = p;
+        if (c == '#')
+        {
+            char buff[255];
+            fgets(buff, 255, inputFile);
+        }
+        else
+        {
+            inputFileID = c - '0'; //To convert '0' --> 0, '1' --> 1 etc.
+            fscanf(inputFile, "\t%d\t%d\t%d\n", &inputFileArrTime, &inputFileRunTime, &inputFilePriority);
+            process p;
+            p.id = inputFileID;
+            p.arrivalTime = inputFileArrTime;
+            p.runTime = inputFileRunTime;
+            p.priority = inputFilePriority;
+            fileProcesses[fileProcessesCount++] = p;
+        }
+        c = fgetc(inputFile);
     }
-    
+
     //NOW we have an array of Processes "fileProcesses" with size "fileProcessesCount" that contains the parameters of each process
     /*
     for (int i = 0; i < fileProcessesCount; i++)
@@ -65,23 +82,28 @@ int main(int argc, char *argv[])
     int Quantum = 0;
     if (Algorithm == 5)
         Quantum = atoi(argv[3]);
-  
+
     switch (Algorithm)
     {
-    case 1:  printf(" Scheduled Algorithm is First Come First Serve (FCFS)\n");
-         break;
-    case 2:  printf("Scheduled Algorithm isShortest Job First (SJF)\n");
+    case 1:
+        printf(" Scheduled Algorithm is First Come First Serve (FCFS)\n");
         break;
-    case 3: printf("Scheduled Algorithm is Preemptive Highest Priority First (HPF) \n");
+    case 2:
+        printf("Scheduled Algorithm isShortest Job First (SJF)\n");
         break;
-    case 4: printf("Scheduled Algorithm isShortest Remaining \n");
+    case 3:
+        printf("Scheduled Algorithm is Preemptive Highest Priority First (HPF) \n");
         break;
-    case 5:  printf("Scheduled Algorithm is Round Robin\n");
+    case 4:
+        printf("Scheduled Algorithm isShortest Remaining \n");
+        break;
+    case 5:
+        printf("Scheduled Algorithm is Round Robin\n");
         break;
     }
-    
+
     // 3. Initiate and create the scheduler and clock processes.
-    
+
     int clk_Pid = fork();
     if (clk_Pid == -1)
         return -1;
@@ -97,10 +119,10 @@ int main(int argc, char *argv[])
     {
         //scheduler
     }
-    
+
     // 4. Use this function after creating the clock process to initialize clock.
     initClk();
-    // To get time use this function. 
+    // To get time use this function.
     int x = getClk();
     printf("Current Time is %d\n", x);
     // TODO Generation Main Loop
